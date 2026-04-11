618afcc6d4073394bd09dfeeee5f38464f864080
roger luz <rogerfluz@live.com>
2026-04-11 20:04:19 -0300

    alterado tudo

---
+++ calculadora copy.py
@@ -0,0 +1,21 @@
+import math
+import keyboard
+
+op = ""
+expressao = ""
+n = ""
+
+
+while True:
+    if keyboard.read_key("c"):
+      n = str(input("digite:"))
+      expressao = expressao + n
+print(expressao)
+      
+        
+        
+  
+
+
+
+    
