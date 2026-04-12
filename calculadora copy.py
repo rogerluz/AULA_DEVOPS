3be5bc2ab3eac984c76a12eff72f22f77c41096b
roger luz <rogerfluz@live.com>
2026-04-12 19:41:34 -0300

    feito no pycharm

--- calculadora copy.py
+++ calculadora copy.py
@@ -1,16 +1,33 @@
-import math
 import keyboard
-
 op = ""
-expressao = ""
-n = ""
-
+numb1 = ""
+numb2 = ""
+final = 0
 
 while True:
-    if keyboard.read_key("c"):
-      n = str(input("digite:"))
-      expressao = expressao + n
-print(expressao)
+    tecla = keyboard.read_key()
+    if tecla != "c":
+        if tecla not in ['s', 'd', 'c'] and op == "":
+            numb1 += tecla
+            continue
+        elif tecla in ['s', 'd']:
+            op = tecla
+        elif tecla not in ['s', 'd', 'c']:
+            numb2 += tecla
+            continue
+    else:
+        match op:
+            case 's':
+                n1 = int(numb1)
+                n2 = int(numb2)
+                final = n1 + n2
+                break
+            case 'd':
+                n1 = int(numb1)
+                n2 = int(numb2)
+                final = n1 - n2
+                break
+print(final)
       
         
         
